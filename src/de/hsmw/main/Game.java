3b5cb776d6480e77febd3a4f84b052c64217901f
PaulFuchs <pfuchs480@gmail.com>
2021-06-14 16:52:29 +0200

    :tada: added first files

---
+++ src/de/hsmw/main/Game.java
@@ -0,0 +1,82 @@
+package de.hsmw.main;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable {
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+    private Thread thread;
+    private boolean running = false;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "newGame", this);
+        running = true;
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        g.dispose();
+        bs.show();
+    }
+
+    private void tick() {
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+}
