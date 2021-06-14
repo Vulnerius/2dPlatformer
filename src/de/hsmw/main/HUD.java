e098ceeb65ef7c2671d94d0545ac349e2ed5757e
PaulFuchs <pfuchs480@gmail.com>
2021-06-14 23:14:38 +0200

    :tada: healthbar goes down

--- src/de/hsmw/main/HUD.java
+++ src/de/hsmw/main/HUD.java
@@ -6,7 +6,7 @@ public class HUD {
     public static int HEALTH = 100;
 
     public void tick(){
-        HEALTH--;
+        HEALTH-=1;
         HEALTH = Game.clamp(HEALTH,0,100);
     }
 
@@ -14,8 +14,6 @@ public class HUD {
         g.setColor(Color.darkGray);
         g.fillRect(15,15,200,32);
         g.setColor(Color.green);
-        g.fillRect(15,15,HEALTH*2,32);
-        g.setColor(Color.WHITE);
-        g.fillRect(15,15,200,32);
+        g.fillRect(15,15,HEALTH,32);
     }
 }
