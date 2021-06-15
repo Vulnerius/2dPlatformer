8d74d183c70a720afafc610a4b9abe003da558fa
PaulFuchs <pfuchs480@gmail.com>
2021-06-15 04:34:35 +0200

    :fire: deleted jframe setLocation

--- src/de/hsmw/main/Window.java
+++ src/de/hsmw/main/Window.java
@@ -12,7 +12,6 @@ public class Window extends Canvas {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
         frame.add(game);
         frame.setVisible(true);
         game.start();
